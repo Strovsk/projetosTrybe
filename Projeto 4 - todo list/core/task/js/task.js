91bcec67196f64eb0a2a5e0f588c6167a26b17ad
Strovsk <strovsk@outlook.com>
2021-12-24 20:24:46 -0300

    adicionados links de classes para cada elemento criado na task e erro de adição de filho removido

--- Projeto 4 - todo list/core/task/js/task.js
+++ Projeto 4 - todo list/core/task/js/task.js
@@ -11,6 +11,11 @@ class Task {
     this.dateElm;
     this.completeElm;
     this.descriptionElm;
+    this.iconsContainerElm;
+    this.trashContainerElm;
+    this.trashElm;
+    this.doneContainerElm;
+    this.doneElm;
 
     this.genElement();
   }
@@ -38,18 +43,47 @@ class Task {
   }
   genTopTitleDate() {
     this.titleDateElm = document.createElement('section');
+    this.titleDateElm.classList.add('task-date');
     this.titleElm = document.createElement('h1');
     this.titleElm.innerText = this.title;
     this.dateElm = document.createElement('p');
+    this.dateElm.classList.add('date');
     this.dateElm.innerText = this.formatDate();
     this.titleDateElm.appendChild(this.titleElm);
     this.titleDateElm.appendChild(this.dateElm);
   }
-  genMidCompleteDescription () {
+  genMidCompleteDescription() {
     this.completeElm = document.createElement('p');
     this.completeElm.innerText = 'Tarefa Completa!';
+    this.completeElm.classList.add('completed-task')
     this.descriptionElm = document.createElement('p');
+    this.descriptionElm.classList.add('description');
     this.descriptionElm.innerText = this.description;
+  }
+  genBottomIcons() {
+    this.trashContainerElm = document.createElement('span');
+    this.trashContainerElm.classList.add('icon-background');
+    this.trashElm = document.createElement('object');
+    this.trashElm.classList.add('icon');
+    this.trashElm.setAttribute('data', './core/task/assets/svg/trash.svg');
+    this.trashElm.setAttribute('type', 'image/svg+xml');
+    // this.trashElm.setAttribute('data', '../assets/svg/trash.svg');
+
+    this.doneContainerElm = document.createElement('span');
+    this.doneContainerElm.classList.add('icon-background');
+    this.doneElm = document.createElement('object');
+    this.doneElm.setAttribute('data', './core/task/assets/svg/done.svg');
+    this.doneElm.setAttribute('type', 'image/svg+xml');
+    this.doneElm.classList.add('icon');
+    // this.doneElm.setAttribute('data', '../assets/svg/done.svg');
+
+    this.trashContainerElm.appendChild(this.trashElm);
+    this.doneContainerElm.appendChild(this.doneElm);
+
+    this.iconsContainerElm = document.createElement('section');
+    this.iconsContainerElm.appendChild(this.trashContainerElm);
+    this.iconsContainerElm.appendChild(this.doneContainerElm);
+    this.iconsContainerElm.classList.add('icons');
   }
   genElement() {
     this.containerElm = document.createElement('li');
@@ -60,6 +94,9 @@ class Task {
     this.genMidCompleteDescription();
     this.containerElm.appendChild(this.completeElm);
     this.containerElm.appendChild(this.descriptionElm);
+
+    this.genBottomIcons();
+    this.containerElm.appendChild(this.iconsContainerElm);
   }
   updateElement() {
     if (elm === null || elm === undefined) reuturn;
