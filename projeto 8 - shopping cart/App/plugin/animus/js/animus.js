84063ad42f98f8e3f223ad0326c7d5de38d219b2
Strovsk <strovsk@outlook.com>
2022-03-07 21:10:42 -0300

    component SaleProduct - plugin animus adicionado

---
+++ projeto 8 - shopping cart/App/plugin/animus/js/animus.js
@@ -0,0 +1,64 @@
+import { Ball } from "./entity/ball.js";
+import { Vector } from "./vector.js";
+
+const canvasElm = document.getElementById('mini-ballCvs');
+const canvas = canvasElm.getContext('2d');
+const canvasDimensions = () => canvasElm.getBoundingClientRect();
+
+const gravity = new Vector(0.2, 270);
+const impulse = new Vector(0.2, 60);
+const fr = new Vector(.1, 0);
+
+const randX = () => (280 * Math.random());
+const vecRand = () => 1 + (5 * Math.random());
+const vecRand180 = () => Math.round(30 + (180 * Math.random()));
+const radiusRand = () => 1 + (5 * Math.random());
+const angleRand = () => (360 * Math.random());
+const colorRand = () => ({
+    0: '#1b1b1b',
+    1: '#2C2A63',
+    2: '#74B2A8',
+  })[Math.round(2 * Math.random())];
+const ballsList = [];
+for (let index = 0; index < 100; index += 1) {
+  let buffer = new Ball(randX(), -10, vecRand(), 10, 2, angleRand(), radiusRand(), colorRand(), '#mini-ballCvs');
+  buffer.vec.addForce('g', gravity);
+  buffer.vec.addForce('imp', impulse);
+  buffer.vec.getForces()['imp'].setAngle(vecRand180());
+  buffer.vec.addForce('friction', fr);
+
+  ballsList.push(buffer);
+}
+
+const getDt = (now) => {
+  let dt = Math.min(1, ( now - lastTime ) / 1000 );
+  // console.log(now, lastTime, now-lastTime);
+  lastTime = now;
+  return dt;
+};
+
+function draw(dt) {
+  ballsList.forEach((elm) => {
+    elm.draw();
+  });
+}
+
+function update(dt) {  
+  ballsList.forEach((elm) => {
+    elm.update();
+    elm.vec.applyForce('g');
+    elm.vec.applyForce('imp');
+    elm.onColisionWall().all();
+  });
+}
+let lastTime = 0;
+let dt = 0;
+function main(timeElapsed) {
+  dt = getDt(timeElapsed);
+  canvas.clearRect(0, 0, canvasDimensions().width, canvasDimensions().height);
+  update(dt);
+  draw(dt);
+  requestAnimationFrame(main);
+};
+
+main();
