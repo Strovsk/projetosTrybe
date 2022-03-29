c919a1fee692644a6cbaf565812c94199fc2fa64
Strovsk <strovsk@outlook.com>
2022-03-28 23:38:19 -0300

    Oferta adicionada

--- projeto 8 - shopping cart/App/core/services/api.js
+++ projeto 8 - shopping cart/App/core/services/api.js
@@ -2,3 +2,13 @@ async function getCategories() {
   const prList = await fetch('https://api.mercadolibre.com/sites/MLB/categories#json').then(response => response.json());
   return prList;
 }
+
+async function getProductsList(categoryId) {
+  const prList = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`).then(result => result.json());
+  return prList;
+}
+
+async function searchProduct(search) {
+  const prList = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`).then(result => result.json());
+  return prList;
+}
--- projeto 8 - shopping cart/App/script.js
+++ projeto 8 - shopping cart/App/script.js
@@ -1,4 +1,5 @@
-(async () => { // adicionando uma categoria
+// NOTE adicionando uma categoria
+(async () => { 
   const categoriesList = await getCategories();
   let buffer;
   console.log(categoriesList);
@@ -9,6 +10,7 @@
   }
 })();
 
+// NOTE adicoinando item ao carrinho
 (() => {
   const itemListElm = document.getElementsByClassName('itemList')[0];
   for (let index = 0; index < 6; index += 1) {
@@ -18,8 +20,30 @@
   // itemListElm.appendChild(item.getContainer());
 })();
 
-(() => { // adicionando item ao carrinho
+// NOTE adicionando item ao carrinho
+(() => {
   const containerCart = document.getElementsByClassName('cart-itemSection')[0];
   const testItemCart = new itemCart('MLB');
   containerCart.appendChild(testItemCart.getContainer());
+})();
+
+// NOTE função para carregar a lista de produtos
+
+
+// NOTE adicionando a oferta
+(async () => {
+  const categoriesList = await getCategories();
+  let firstItem = await getProductsList(categoriesList[0].id);
+  console.log(firstItem.results[0]);
+  firstItem = firstItem.results[0];
+  const containerOfferElm = document.getElementsByClassName('saleProduct-textContent')[0];
+
+  const titleProductElm = document.getElementsByClassName('saleProduct-nameContent')[0].children[0];
+  titleProductElm.innerText = firstItem.title;
+
+  const priceElm = document.getElementById('saleProduct-priceContent').children[1];
+  priceElm.innerText = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(firstItem.price);
+
+  const imageElm = document.getElementsByClassName('saleProduct-image')[0];
+  imageElm.src = firstItem.thumbnail;
 })();
