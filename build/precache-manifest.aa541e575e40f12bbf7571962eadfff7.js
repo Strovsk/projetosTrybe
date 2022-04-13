6a36efd7c54cdf70eb6a89b3b1fa760e7bba9cba
Thiago Santa Clara <strovsk@outlook.com>
2022-04-13 16:19:11 -0300

    mobile impl

---
+++ build/precache-manifest.aa541e575e40f12bbf7571962eadfff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a5687870ce932185df09bd315313e26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70692141ffe4d5772c45",
+    "url": "/static/css/main.b799bee3.chunk.css"
+  },
+  {
+    "revision": "d9669645ad7a3f0900d5",
+    "url": "/static/js/2.cec15f0e.chunk.js"
+  },
+  {
+    "revision": "db83c342ba5454d01e66df1b8dcb6c26",
+    "url": "/static/js/2.cec15f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70692141ffe4d5772c45",
+    "url": "/static/js/main.b28ae464.chunk.js"
+  },
+  {
+    "revision": "f8a4e7c1de7ec49c0042",
+    "url": "/static/js/runtime-main.e2209b7b.js"
+  }
+]);
